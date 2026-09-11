787a279daf3b34ff75c5883651d50eab02ae0bf0
Rashad Gover <phantomtype@protonmail.com>
2026-09-11 03:42:27 +0000

    Add GovernanceLock contract filter
    Signed-off-by: Rashad Gover <phantomtype@protonmail.com>

--- apps/sv/src/main/scala/org/lfdecentralizedtrust/splice/sv/store/SvDsoStore.scala
+++ apps/sv/src/main/scala/org/lfdecentralizedtrust/splice/sv/store/SvDsoStore.scala
@@ -1680,6 +1680,26 @@ object SvDsoStore {
           contractExpiresAt = Some(Timestamp.assertFromInstant(contract.payload.endTime)),
         )
       },
+      mkFilter(splice.governancelock.GovernanceLock.COMPANION)(
+        co => co.payload.dso == dso,
+        versionGuard = { case (pkgVersionSupport, now) =>
+          (tc) => pkgVersionSupport.supportsGovernanceLock(Seq(dsoParty), now)(tc)
+        },
+      ) { contract =>
+        contract.payload.specification.kind match {
+          case kind: splice.governancelock.governancelockkind.GLK_SuperValidatorRightsOwner =>
+            DsoAcsStoreRowData(
+              contract,
+              svName = Some(kind.svName),
+            )
+          case kind: splice.governancelock.governancelockkind.GLK_FeaturedApp =>
+            DsoAcsStoreRowData(
+              contract,
+              featuredAppRightProvider = Some(PartyId.tryFromProtoPrimitive(kind.provider)),
+            )
+          case other => sys.error(s"Unexpected GovernanceLockKind: $other")
+        }
+      },
     )
 
     MultiDomainAcsStore.SimpleContractFilter(
