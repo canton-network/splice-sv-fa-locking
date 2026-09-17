cbe16e307d1927c2d1291a794af1500dcc2cbbd8
Rashad Gover <phantomtype@protonmail.com>
2026-09-17 07:20:12 -0700

    Update apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V074__dso_acs_store_provisional_featured_app_lock_for.sql
    Co-authored-by: Simon Meier <simon@digitalasset.com>
    Signed-off-by: Rashad Gover <phantomtype@protonmail.com>

--- apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V074__dso_acs_store_provisional_featured_app_lock_for.sql
+++ apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V074__dso_acs_store_provisional_featured_app_lock_for.sql
@@ -2,5 +2,5 @@ alter table dso_acs_store
   add column provisional_featured_app_lock_for text;
 
 create index dso_acs_store_provisional_featured_app_lock_for
-    on dso_acs_store (store_id, migration_id, template_id_qualified_name, provisional_featured_app_lock_for)
+    on dso_acs_store (store_id, migration_id, provisional_featured_app_lock_for)
     where provisional_featured_app_lock_for is not null;
