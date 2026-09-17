31f6b17149f2546b4ce5afd919f2fb46b50a61dc
Matt Dziuban <mrdziuban@gmail.com>
2026-09-17 18:53:33 +0000

    Test partial and full withdrawal via TSv1.
    Signed-off-by: Matt Dziuban <mrdziuban@gmail.com>

--- apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/GovernanceLockTimeBasedIntegrationTest.scala
+++ apps/app/src/test/scala/org/lfdecentralizedtrust/splice/integration/tests/GovernanceLockTimeBasedIntegrationTest.scala
@@ -3,11 +3,15 @@
 
 package org.lfdecentralizedtrust.splice.integration.tests
 
-import org.lfdecentralizedtrust.splice.codegen.java.splice.governancelock
+import com.digitalasset.canton.config.NonNegativeFiniteDuration
+import org.lfdecentralizedtrust.splice.codegen.java.splice.{amulet as amuletCodegen, governancelock}
+import org.lfdecentralizedtrust.splice.config.ConfigTransforms
 import org.lfdecentralizedtrust.splice.console.LedgerApiExtensions.RichPartyId
 import org.lfdecentralizedtrust.splice.integration.EnvironmentDefinition
 import org.lfdecentralizedtrust.splice.integration.tests.SpliceTests.IntegrationTestWithIsolatedEnvironment
 import org.lfdecentralizedtrust.splice.util.{TimeTestUtil, WalletTestUtil}
+
+import java.time.Duration
 
 class GovernanceLockTimeBasedIntegrationTest
     extends IntegrationTestWithIsolatedEnvironment
@@ -18,6 +22,17 @@ class GovernanceLockTimeBasedIntegrationTest
   override def environmentDefinition: SpliceEnvironmentDefinition =
     EnvironmentDefinition
       .simpleTopology1SvWithSimTime(this.getClass.getSimpleName)
+      .addConfigTransforms((_, config) =>
+        ConfigTransforms.updateAllSvAppFoundDsoConfigs_(
+          _.copy(
+            // Shorten the SV vesting period and search time granularity so we can test partial and
+            // full withdrawal while only advancing the clock a couple minutes
+            initialGovernanceLockSuperValidatorLockVestingDuration =
+              Some(NonNegativeFiniteDuration.ofSeconds(5)),
+            initialGovernanceLockSearchTimeGranularity = Some(NonNegativeFiniteDuration.ofMicros(1)),
+          )
+        )(config)
+      )
 
   private def matchSVKind(kind: governancelock.GovernanceLockKind): Unit =
     kind match {
@@ -73,7 +88,7 @@ class GovernanceLockTimeBasedIntegrationTest
           .disclosedContracts should not be empty
       }
 
-      // Advance sim-time past the lock's createdAt time
+      // Advance sim-time past the lock's requestedAt time
       advanceTimeAndWaitForRoundOpening
 
       // Withdraw the governance lock; it's relocked as a VestingLock and funds remain locked
@@ -115,6 +130,77 @@ class GovernanceLockTimeBasedIntegrationTest
 
       clue("the amulet remains locked while vesting") {
         aliceWalletClient.balance().lockedQty should beAround(lockAmount)
+      }
+
+      // Advance 2.5 seconds into the 5 second vesting period and withdraw
+      advanceTime(Duration.ofMillis(2500))
+
+      val (_, (remainingVestingLockCid, remainingVestingAmount)) = actAndCheck(
+        "the owner withdraws the VestingLock mid-vesting",
+        withdrawTransferInstruction(
+          aliceValidatorBackend.participantClientWithAdminToken,
+          owner,
+          vestingLockCid,
+        ),
+      )(
+        "a new VestingLock remains with half of the total vesting amount",
+        _ => {
+          val (cid, view) = listTransferInstructions(
+            aliceValidatorBackend.participantClientWithAdminToken,
+            ownerParty,
+          ).loneElement
+          val remainingVestingLock =
+            aliceValidatorBackend.participantClientWithAdminToken.ledger_api_extensions.acs
+              .filterJava(governancelock.VestingLock.COMPANION)(
+                ownerParty,
+                predicate = _.id.contractId == cid.contractId,
+              )
+              .loneElement
+              .data
+          cid.contractId should not be vestingLockCid.contractId
+          view.transfer.sender shouldBe ownerParty.toProtoPrimitive
+          view.transfer.receiver shouldBe superValidatorLockMagicParty.toProtoPrimitive
+          matchSVKind(remainingVestingLock.specification.kind)
+          val remainingVestingAmount = BigDecimal(remainingVestingLock.vestingAmount)
+          remainingVestingAmount should beAround(lockAmount / 2)
+          aliceWalletClient.balance().lockedQty should beAround(lockAmount / 2)
+          (cid, remainingVestingAmount)
+        },
+      )
+
+      // Advance past the end of the vesting period and withdraw
+      advanceTime(Duration.ofSeconds(5))
+
+      actAndCheck(
+        "the owner withdraws the fully-vested VestingLock",
+        withdrawTransferInstruction(
+          aliceValidatorBackend.participantClientWithAdminToken,
+          owner,
+          remainingVestingLockCid,
+        ),
+      )(
+        "the VestingLock is archived and no pending TransferInstruction remains",
+        _ => {
+          listTransferInstructions(
+            aliceValidatorBackend.participantClientWithAdminToken,
+            ownerParty,
+          ) shouldBe empty
+          aliceValidatorBackend.participantClientWithAdminToken.ledger_api_extensions.acs
+            .filterJava(governancelock.VestingLock.COMPANION)(ownerParty, _ => true) shouldBe empty
+        },
+      )
+
+      clue("the full withdraw returns the LockedAmulet to the owner as an expired lock") {
+        val now = getLedgerTime.toInstant
+        val remainingLock =
+          aliceValidatorBackend.participantClientWithAdminToken.ledger_api_extensions.acs
+            .filterJava(amuletCodegen.LockedAmulet.COMPANION)(ownerParty, _ => true)
+            .loneElement
+            .data
+        remainingLock.lock.expiresAt.isAfter(now) shouldBe false
+        BigDecimal(remainingLock.amulet.amount.initialAmount) should beAround(
+          remainingVestingAmount
+        )
       }
   }
 }
